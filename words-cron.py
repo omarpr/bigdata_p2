a3e15c3e5ddc22348a470b4c2f03925831db8289
omarpr <omarpr@gmail.com>
2017-05-21 23:18:54 -0400

    More work.

--- words-cron.py
+++ words-cron.py
@@ -4,58 +4,10 @@
 from pyspark import SparkContext
 from datetime import datetime, timezone, timedelta
 import calendar, time, os, json
+from p2lib import *
 
 sc = SparkContext(appName="p2-words-cron")
 sc.setLogLevel("WARN")
-
-def resultToFiles(drd, file_dir, data_name, time_freq):
-    out = {}
-
-    f_index = file_dir + data_name + '-index.json'
-    f_index_change = False
-    ws_index = {}
-
-    if (time_freq == '1h'):
-        freq = timedelta(hours=1)
-    elif (time_freq == '10m'):
-        freq = timedelta(minutes=10)
-    else:
-        freq = timedelta()
-
-    if (os.path.isfile(f_index)):
-        with open(f_index, 'r') as f:
-             ws_index = json.load(f)
-
-    for x in drd.collect():
-        ts = int(time.mktime(x[0][1].timetuple()))
-        if (ts not in out):
-            out[ts] = []
-        out[ts].append((x[0][0], x[1]))
-
-    for x in out:
-        f = file_dir + data_name + '.txt-' + str(x)
-
-        if (os.path.isfile(f)):
-            os.remove(f)
-
-        target_file = os.open(f, os.O_RDWR|os.O_CREAT)
-        os.write(target_file, str.encode(json.dumps(out[x])))
-        os.close(target_file)
-
-        if (x not in ws_index):
-            tstime = datetime.fromtimestamp(x)
-            drange = tstime.strftime('%d-%b-%Y %I:%M%p') + ' - ' + (tstime + freq).strftime('%d-%b-%Y %I:%M%p')
-            f_index_change = True
-            ws_index[x] = {'ts': x, 'file': data_name + '.txt-' + str(x), 'date_range': drange}
-
-
-    if (f_index_change):
-        if (os.path.isfile(f_index)):
-            os.remove(f_index)
-
-        f = open(f_index, 'w+')
-        f.write(json.dumps(ws_index))
-        f.close()
 
 main_dir = '/home/omar.soto2/p2/files/'
 words_selection = ['TRUMP', 'MAGA', 'DICTATOR', 'IMPEACH', 'DRAIN', 'SWAMP']
