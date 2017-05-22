17c4fc7c656b28b72ac22189492ae4eee472bc6c
omarpr <omarpr@gmail.com>
2017-05-21 23:20:17 -0400

    New files added.

---
+++ p2lib.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timezone, timedelta
+import calendar, time, os, json
+
+def resultToFiles(drd, file_dir, data_name, time_freq):
+    out = {}
+
+    f_index = file_dir + data_name + '-index.json'
+    f_index_change = False
+    ws_index = {}
+
+    if (time_freq == '12h'):
+        freq = timedelta(hours=12)
+    elif (time_freq == '1h'):
+        freq = timedelta(hours=1)
+    elif (time_freq == '10m'):
+        freq = timedelta(minutes=10)
+    else:
+        freq = timedelta()
+
+    if (os.path.isfile(f_index)):
+        with open(f_index, 'r') as f:
+             ws_index = json.load(f)
+
+    for x in drd.collect():
+        ts = int(time.mktime(x[0][1].timetuple()))
+        if (ts not in out):
+            out[ts] = []
+        out[ts].append((x[0][0], x[1]))
+
+    for x in out:
+        f = file_dir + data_name + '.txt-' + str(x)
+
+        if (os.path.isfile(f)):
+            os.remove(f)
+
+        target_file = os.open(f, os.O_RDWR|os.O_CREAT)
+        os.write(target_file, str.encode(json.dumps(out[x])))
+        os.close(target_file)
+
+        if (x not in ws_index):
+            f_index_change = True
+
+            tstime = datetime.fromtimestamp(x)
+
+            drange_low = tstime.strftime('%d-%b-%Y %I:%M%p')
+            drange_top = (tstime + freq).strftime('%d-%b-%Y %I:%M%p')
+            drange = drange_low + ' - ' + drange_top
+
+            ws_index[x] = {'ts': x, 'file': data_name + '.txt-' + str(x), 'date_range': drange, 'drange_low': drange_low, 'drange_top': drange_top}
+
+
+    if (f_index_change):
+        if (os.path.isfile(f_index)):
+            os.remove(f_index)
+
+        f = open(f_index, 'w+')
+        f.write(json.dumps(ws_index))
+        f.close()
---
+++ screennames-cron.py
@@ -0,0 +1,26 @@
+# pyspark --master yarn --deploy-mode client --conf='spark.executorEnv.PYTHONHASHSEED=223'
+# /opt/spark/bin/spark-submit screennames-cron.py
+
+from pyspark import SparkContext
+from datetime import datetime, timezone, timedelta
+import calendar, time, os, json
+from p2lib import *
+
+sc = SparkContext(appName="p2-screennames-cron")
+sc.setLogLevel("WARN")
+
+main_dir = '/home/omar.soto2/p2/files/'
+
+a = sc.textFile('/home/omar/p2/screen_names.txt-*/part-*')
+b = a.map(lambda x: eval(x))
+c = b.map(lambda x: ((x[0][0].upper(), (datetime.strptime(x[0][1].split('+')[0], '%Y-%m-%dT%H:%M:%S') - timedelta(seconds=time.timezone))), x[1]))
+
+map_12h = c.map(lambda x: ((x[0][0], x[0][1] - timedelta(minutes=x[0][1].minute) - timedelta(hours=12)), x[1]))
+
+e4c = map_12h
+e4c = e4c.filter(lambda x: (x[0][1] + timedelta(seconds=time.timezone)) >= datetime.utcnow() - timedelta(hours=12))
+e4c_reduce = e4c.reduceByKey(lambda x, y: x+y)
+
+print("Count: " + str(e4c.count()))
+
+resultToFiles(e4c_reduce, main_dir, 'screen_names', '12h')
